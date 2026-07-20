fa49254782e337d019057888c7e04854e16e3725
Barnabás Mórocz <mester3210@gmail.com>
2026-07-20 19:46:02 +0200

    update dist/main

--- FnLib/dist/main.js
+++ FnLib/dist/main.js
@@ -55,10 +55,52 @@
     }
   }
 
+  // src/fn_lib__array2d.js
+  class FnLib__Array2D {
+    static create(size, fill = null) {
+      return Array.from({ length: size.y }, (_, y) => Array.from({ length: size.x }, (_2, x) => typeof fill == "function" ? fill(x, y) : fill));
+    }
+    static getSizeX(arr) {
+      return arr[0]?.length ?? 0;
+    }
+    static getSizeY(arr) {
+      return arr.length;
+    }
+    static _extreme(arr, fn, compare, indexes) {
+      if (fn == null)
+        fn = (x) => x;
+      const sizeX = this.getSizeX(arr);
+      const sizeY = this.getSizeY(arr);
+      let extreme = fn(arr[0][0]);
+      let index_list = indexes ? [{ x: 0, y: 0 }] : null;
+      for (let y = 0;y < sizeY; y++) {
+        for (let x = 0;x < sizeX; x++) {
+          if (x == 0 && y == 0)
+            continue;
+          const value = fn(arr[y][x]);
+          if (compare(value, extreme)) {
+            extreme = value;
+            index_list = indexes ? [{ x, y }] : null;
+          } else if (indexes && value == extreme) {
+            index_list.push({ x, y });
+          }
+        }
+      }
+      return indexes ? { value: extreme, indexes: index_list } : extreme;
+    }
+    static min(arr, fn = null, indexes = false) {
+      return this._extreme(arr, fn, (a, b) => a < b, indexes);
+    }
+    static max(arr, fn = null, indexes = false) {
+      return this._extreme(arr, fn, (a, b) => a > b, indexes);
+    }
+  }
+
   // src/fn_lib.js
   class FnLib {
     static Math = FnLib__Math;
     static Array1D = FnLib__Array1D;
+    static Array2D = FnLib__Array2D;
   }
 
   // src/main.js
